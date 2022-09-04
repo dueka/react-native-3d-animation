a7b6c5caaf192df699d6302ddf0e1ff6bf9f250a
Ekanem David <38921132+dueka@users.noreply.github.com>
2022-09-04 20:36:26 +0100

    create

--- App.tsx
+++ App.tsx
@@ -1,79 +1,201 @@
-import React from "react";
-import { View } from "react-native";
-import Expo from "expo";
+import * as React from "react";
 import {
-  Scene,
-  Mesh,
-  MeshBasicMaterial,
-  PerspectiveCamera,
-  BoxBufferGeometry,
-} from "three";
-import ExpoTHREE, { Renderer } from "expo-three";
-import { ExpoWebGLRenderingContext, GLView } from "expo-gl";
+  FlatList,
+  Image,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+  StyleSheet,
+  Animated,
+} from "react-native";
+import { SafeAreaView } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import faker from "faker";
 import { StatusBar } from "expo-status-bar";
+const { width, height } = Dimensions.get("screen");
 
-const App = () => {
-  const onContextCreate = async (gl: any) => {
-    // three.js implementation.
-    const scene = new Scene();
-    const camera = new PerspectiveCamera(
-      75,
-      gl.drawingBufferWidth / gl.drawingBufferHeight,
-      0.1,
-      1000
-    );
-    gl.canvas = {
-      width: gl.drawingBufferWidth,
-      height: gl.drawingBufferHeight,
-    };
+const IMAGE_WIDTH = width * 0.65;
+const IMAGE_HEIGHT = height * 0.7;
+const SPACING = 20;
 
-    // set camera position away from cube
-    camera.position.z = 2;
+const images = [
+  "https://images.pexels.com/photos/1799912/pexels-photo-1799912.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/1769524/pexels-photo-1769524.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/1758101/pexels-photo-1758101.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/1738434/pexels-photo-1738434.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/1698394/pexels-photo-1698394.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/1684429/pexels-photo-1684429.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/1690351/pexels-photo-1690351.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/1668211/pexels-photo-1668211.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/1647372/pexels-photo-1647372.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/1616164/pexels-photo-1616164.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/1799901/pexels-photo-1799901.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/1789968/pexels-photo-1789968.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/1774301/pexels-photo-1774301.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/1734364/pexels-photo-1734364.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://images.pexels.com/photos/1724888/pexels-photo-1724888.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+];
 
-    const renderer = new Renderer({ gl });
-    // set size of buffer to be equal to drawing buffer width
-    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
-
-    // create cube
-    // define geometry
-    const geometry = new BoxBufferGeometry(1, 1, 1);
-    const material = new MeshBasicMaterial({
-      color: "cyan",
-    });
-
-    const cube = new Mesh(geometry, material);
-
-    // add cube to scene
-    scene.add(cube);
-
-    // create render function
-    const render = () => {
-      requestAnimationFrame(render);
-
-      // create rotate functionality
-      // rotate around x axis
-      cube.rotation.x += 0.01;
-
-      // rotate around y axis
-      cube.rotation.y += 0.01;
-
-      renderer.render(scene, camera);
-      gl.endFrameEXP();
-    };
-
-    // call render
-    render();
+faker.seed(10);
+const DATA = [...Array(images.length).keys()].map((_, i) => {
+  return {
+    key: faker.datatype.uuid(),
+    image: images[i],
+    title: faker.commerce.product(),
+    subtitle: faker.company.bs(),
+    price: faker.finance.amount(80, 200, 0),
   };
+});
 
+const Content = ({ item }: any) => {
   return (
-    <View>
-      <GLView
-        onContextCreate={onContextCreate}
-        // set height and width of GLView
-        style={{ width: 400, height: 400 }}
-      />
+    <>
+      <Text
+        style={{
+          textAlign: "center",
+          fontWeight: "800",
+          fontSize: 16,
+          textTransform: "uppercase",
+        }}
+        numberOfLines={1}
+        adjustsFontSizeToFit
+      >
+        {item?.title}
+      </Text>
+      <Text style={{ fontSize: 12, opacity: 0.4 }}>{item.subtitle}</Text>
+    </>
+  );
+};
+export default () => {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+  const progress = Animated.modulo(Animated.divide(scrollX, width), width);
+  return (
+    <View style={{ backgroundColor: "#A5F1FA", flex: 1 }}>
+      <StatusBar hidden />
+      <SafeAreaView style={{ marginTop: SPACING * 1 }}>
+        <View style={{ height: IMAGE_HEIGHT * 2.1 }}>
+          <Animated.FlatList
+            data={DATA}
+            keyExtractor={(item) => item.key}
+            horizontal
+            pagingEnabled
+            onScroll={Animated.event(
+              [
+                {
+                  nativeEvent: { contentOffset: { x: scrollX } },
+                },
+              ],
+              {
+                useNativeDriver: true,
+              }
+            )}
+            bounces={false}
+            style={{ flexGrow: 0, zIndex: 9999 }}
+            contentContainerStyle={{
+              height: IMAGE_HEIGHT + SPACING * 2,
+              paddingHorizontal: SPACING * 4,
+            }}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item, index }) => {
+              const inputRange = [
+                (index - 1) * width, // next slide
+                index * width, // current slide
+                (index + 1) * width, // previous slide
+              ];
+              const opacity = scrollX.interpolate({
+                inputRange,
+                outputRange: [0, 1, 0],
+              });
+              const translateY = scrollX.interpolate({
+                inputRange,
+                outputRange: [50, 0, 20], // create a wave
+              });
+              return (
+                <Animated.View
+                  style={{
+                    width,
+                    paddingVertical: SPACING,
+                    opacity,
+                    transform: [{ translateY }],
+                  }}
+                >
+                  <Image
+                    source={{ uri: item.image }}
+                    style={{
+                      width: IMAGE_WIDTH,
+                      height: IMAGE_HEIGHT,
+                      resizeMode: "cover",
+                    }}
+                  />
+                </Animated.View>
+              );
+            }}
+          />
+          <View
+            style={{
+              width: IMAGE_WIDTH,
+              alignItems: "center",
+              paddingHorizontal: SPACING * 2,
+              marginLeft: SPACING * 2,
+            }}
+          >
+            <Content item={DATA[0]} />
+          </View>
+          <Animated.View
+            style={{
+              width: IMAGE_WIDTH + SPACING * 2,
+              position: "absolute",
+              backgroundColor: "white",
+              backfaceVisibility: true,
+              zIndex: -1,
+              top: SPACING * 2,
+              left: SPACING,
+              bottom: 0,
+              shadowColor: "#000",
+              shadowOpacity: 0.2,
+              shadowRadius: 24,
+              shadowOffset: {
+                width: 0,
+                height: 0,
+              },
+              transform: [
+                {
+                  perspective: IMAGE_WIDTH * 4,
+                },
+                {
+                  rotateY: progress.interpolate({
+                    inputRange: [0, 0.5, 1],
+                    outputRange: ["0deg", "90deg", "180deg"],
+                  }),
+                },
+              ],
+            }}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: IMAGE_WIDTH + SPACING * 4,
+            paddingHorizontal: SPACING,
+            paddingVertical: SPACING,
+          }}
+        >
+          <TouchableOpacity onPress={() => {}}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <AntDesign name="swapleft" size={42} color="black" />
+              <Text style={{ fontSize: 12, fontWeight: "800" }}>PREV</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Text style={{ fontSize: 12, fontWeight: "800" }}>NEXT</Text>
+              <AntDesign name="swapright" size={42} color="black" />
+            </View>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
-
-export default App;
---
+++ App1.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { View } from "react-native";
+import Expo from "expo";
+import {
+  Scene,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  BoxBufferGeometry,
+} from "three";
+import ExpoTHREE, { Renderer } from "expo-three";
+import { ExpoWebGLRenderingContext, GLView } from "expo-gl";
+import { StatusBar } from "expo-status-bar";
+
+const App = () => {
+  const onContextCreate = async (gl: any) => {
+    // three.js implementation.
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(
+      75,
+      gl.drawingBufferWidth / gl.drawingBufferHeight,
+      0.1,
+      1000
+    );
+    gl.canvas = {
+      width: gl.drawingBufferWidth,
+      height: gl.drawingBufferHeight,
+    };
+
+    // set camera position away from cube
+    camera.position.z = 2;
+
+    const renderer = new Renderer({ gl });
+    // set size of buffer to be equal to drawing buffer width
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+    // create cube
+    // define geometry
+    const geometry = new BoxBufferGeometry(1, 1, 1);
+    const material = new MeshBasicMaterial({
+      color: "cyan",
+    });
+
+    const cube = new Mesh(geometry, material);
+
+    // add cube to scene
+    scene.add(cube);
+
+    // create render function
+    const render = () => {
+      requestAnimationFrame(render);
+
+      // create rotate functionality
+      // rotate around x axis
+      cube.rotation.x += 0.01;
+
+      // rotate around y axis
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    };
+
+    // call render
+    render();
+  };
+
+  return (
+    <View>
+      {/* <GLView
+        onContextCreate={onContextCreate}
+        // set height and width of GLView
+        style={{ width: 400, height: 400 }}
+      /> */}
+    </View>
+  );
+};
+
+export default App;
+//
